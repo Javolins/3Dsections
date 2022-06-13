52d91db617e19d7f8188932a72ca3a6f9596755b
AleksanderBartoszek <olekbartoszek2711@gmail.com>
2022-06-13 17:57:35 +0200

    one comment

--- include/Triangle.h
+++ include/Triangle.h
@@ -14,18 +14,6 @@
 #include <algorithm>
 #include <string>
 
-//inline std::array<double, 3> cross(std::array<double, 3> a, std::array<double, 3> b){
-//	std::array<double, 3> result;
-//	result[0] = a[1]*b[2] - a[2]*b[1];
-//	result[1] = a[2]*b[0] - a[0]*b[2];
-//	result[2] = a[0]*b[1] - a[1]*b[0];
-//	return result;
-//}
-//
-//inline double norm(std::array<double, 3> vec){
-//	return sqrt(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2]);
-//}
-
 class Triangle : public Plane{
 
 	public:
@@ -98,48 +86,27 @@ inline std::ostream& operator<<(std::ostream& s, const Triangle& t){
 	return s << "Triangle:\n" << t.getEdgeA() << t.getEdgeB() << t.getEdgeC() << std::endl;
 }
 
+/**
+ * @brief class for hashing triangles to be used in std::unordered_set.
+ */
 class hashTriangles{
 	public:
 		size_t operator()(const Triangle& t) const{
 			std::array<Point, 3> points{ t.getPointA(), t.getPointB(), t.getPointC() };
 			std::sort(points.begin(), points.end(), comparePoints());
 
-			//std::string toHash =
-			//	std::to_string(points[0].getX()) +
-			//	std::to_string(points[0].getY()) +
-			//	std::to_string(points[0].getZ()) +
-			//	std::to_string(points[1].getX()) +
-			//	std::to_string(points[1].getY()) +
-			//	std::to_string(points[1].getZ()) +
-			//	std::to_string(points[2].getX()) +
-			//	std::to_string(points[2].getY()) +
-			//	std::to_string(points[2].getZ());
+			size_t out =
+				3 * points[0].getX() +
+				5 * points[0].getY() +
+				7 * points[0].getZ() +
+				11 * points[1].getX() +
+				13 * points[1].getY() +
+				17 * points[1].getZ() +
+				19 * points[2].getX() +
+				23 * points[2].getY() +
+				29 * points[2].getZ();
 
-			//		size_t ax = std::hash<float>{}(points[0].getX());
-			//		size_t ay = std::hash<float>{}(points[0].getY());
-			//		size_t az = std::hash<float>{}(points[0].getZ());
-			//		size_t bx = std::hash<float>{}(points[1].getX());
-			//		size_t by = std::hash<float>{}(points[1].getY());
-			//		size_t bz = std::hash<float>{}(points[1].getZ());
-			//		size_t cx = std::hash<float>{}(points[2].getX());
-			//		size_t cy = std::hash<float>{}(points[2].getY());
-			//		size_t cz = std::hash<float>{}(points[2].getZ());
-
-			//size_t a = ((((((((ax ^ (ay << 1)) ^ (az << 1)) ^ (bx << 1)) ^ (by << 1)) ^ (bz << 1)) ^ (cx << 1)) ^ (cy << 1)) ^ (cz << 1));
-			//size_t number = std::hash<std::string>()(toHash);
-
-			//size_t out =
-			//	3 * points[0].getX() +
-			//	5 * points[0].getY() +
-			//	7 * points[0].getZ() +
-			//	11 * points[1].getX() +
-			//	13 * points[1].getY() +
-			//	17 * points[1].getZ() +
-			//	19 * points[2].getX() +
-			//	23 * points[2].getY() +
-			//	29 * points[2].getZ();
-
-			//return out;//number;
+			return out;
 		}
 };
 
